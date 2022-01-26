a2983904f8c51ccc2a9814211975ca2cdb4925c9
Chethansk1666 <chethansk1666@gmail.com>
2022-01-26 14:03:39 +0530

    first commit

---
+++ src/main/java/com/chethansk/demo/MyPersonalWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.chethansk.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalWebsiteApplication.class, args);
+	}
+
+}
